ed97a666b13b961a30390ee32abfce5482f3c3e0
Iván <ivandm2509@gmail.com>
2023-07-09 11:37:06 +0200

    Score + reset game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Imports
 import pygame
 import random
+import neat
+import os
 
 # Initialize pygame
 pygame.init()
@@ -13,6 +15,9 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Doodle - Game")
 
+# Create Sprite Groups
+platform_group = pygame.sprite.Group()
+
 # Set frame rate
 clock = pygame.time.Clock()
 FPS = 60
@@ -21,15 +26,19 @@ FPS = 60
 GRAVITY = 1
 MAX_PLATFORMS = 6
 SCROLL_THRESH = 225
+restart_game = False
 
 # Colours
 WHITE = (255,255,255)
 RED = (255,0,0)
+BLACK = (0,0,0)
+
 # Load images
 background_img = pygame.image.load("img/background.png").convert_alpha()
 doodle_img = pygame.image.load("img/doodle.png").convert_alpha()
 platform_image = pygame.image.load("img/green_plataform.png").convert_alpha()
 blue_platform_image = pygame.image.load("img/blue_plataform.png").convert_alpha()
+
 
 def create_platform(platform):
     p_w = random.randint(40, 60)
@@ -54,6 +63,7 @@ class Doodle():
         self.rect.center = (x, y)
         self.vel_y = 0
         self.flip = False
+        self.score = 0
 
     def move(self):
         # Reset Variables
@@ -91,17 +101,14 @@ class Doodle():
                         self.rect.bottom = platform.rect.top
                         dy = 0
                         self.vel_y = -20
-
-        # Limit Screen Ground
-        if self.rect.bottom + dy > SCREEN_HEIGHT:
-            dy = 0
-            self.vel_y = -20
         
         # Scroll scenario
         if self.rect.top <= SCROLL_THRESH:
             if self.vel_y < 0:
                 # Move platforms opposite to player movement only when going up
                 scroll = -dy
+                # Add score
+                self.score += abs(dy//10)
 
         # Update rectangle position
         self.rect.x += dx
@@ -112,9 +119,14 @@ class Doodle():
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False),(self.rect.x - 20, self.rect.y - 15))
-        # pygame.draw.rect(screen , WHITE, self.rect, 2)
-        # pygame.draw.circle(screen, RED, self.rect.center, 2)
-        # pygame.draw.circle(screen, RED, self.rect.topright, 2)
+
+        # Draw Score, this will be removed later.
+        font = pygame.font.Font("freesansbold.ttf", 20)
+        text = font.render(str(self.score), True, BLACK)
+        text_rect = text.get_rect()
+        text_rect.center = (SCREEN_WIDTH // 2, 20)
+        screen.blit(text, text_rect)
+
 
 # Platform class
 class Platform(pygame.sprite.Sprite):
@@ -149,52 +161,70 @@ class BluePlatform(pygame.sprite.Sprite):
      
         self.rect.x += self.direction  # Move the platform horizontally
         # Change direction if the platform reaches the maximum distance or screen edges
-        if abs(self.rect.x - self.initial_x) >= self.max_distance or self.rect.right >= 800 or self.rect.left <= 0:
+        if abs(self.rect.x - self.initial_x) >= self.max_distance or self.rect.right >= SCREEN_WIDTH or self.rect.left <= 0:
             self.direction *= -1
 
         if self.rect.top > SCREEN_HEIGHT:
             self.kill()
 
 
-# Player Instance
-doodler = Doodle(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
+class Game():
+    # Player Instance
+    doodler = Doodle(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
 
-# Create Sprite Groups
-platform_group = pygame.sprite.Group()
+    # Create starting platform
+    platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100)
+    platform_group.add(platform)
 
-# Create starting platform
-platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100)
-platform_group.add(platform)
+    # Game loop
+    running = True
+    while running:
+        # Setting FPS limit
+        clock.tick(FPS)
 
-# Game loop
-running = True
-while running:
+        if restart_game == False:
+            # Draw Background and score
+            screen.blit(background_img, (0, 0))
 
-    # Setting FPS limit
-    clock.tick(FPS)
+            # Move Character
+            scroll = doodler.move()
 
-    # Draw Background
-    screen.blit(background_img, (0, 0))
+            # Create new platforms
+            if len(platform_group) < MAX_PLATFORMS:
+                platform = create_platform(platform)
+                platform_group.add(platform)
 
-    # Move Character
-    scroll = doodler.move()
+            # Draw Character
+            platform_group.draw(screen)
+            doodler.draw()
+            
+            platform_group.update(scroll)
 
-    # Create new platforms
-    if len(platform_group) < MAX_PLATFORMS:
-        platform = create_platform(platform)
-        platform_group.add(platform)
+            # Restart game if player falls
+            if doodler.rect.top > SCREEN_HEIGHT:
+                restart_game = True 
+            
+        else:
+            # If player falls, restart game resetting all variables
+            restart_game = False
+            doodler.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
+            doodler.vel_y = 0
+            doodler.score = 0
+            platform_group.empty()
+            # Create starting platform
+            platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100)
+            platform_group.add(platform)
 
-    # Draw Character
-    platform_group.draw(screen)
-    doodler.draw()
-    
-    platform_group.update(scroll)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    # Update Display
-    pygame.display.update()
+        # Update Display
+        pygame.display.update()
 
-pygame.quit()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    Game()
